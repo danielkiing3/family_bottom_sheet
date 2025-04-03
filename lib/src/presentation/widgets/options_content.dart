9a6e83f6447a396bae5d071acac2f3a5fcb2de19
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-04-04 00:17:07 +0100

    refactor : Use FamilyBottomSheetShell and simplify button structure

--- lib/src/presentation/widgets/options_content.dart
+++ lib/src/presentation/widgets/options_content.dart
@@ -1,6 +1,6 @@
-import 'package:family_bottom_sheet/src/common/button/back_button.dart';
-import 'package:family_bottom_sheet/src/common/button/custom_elevated_button.dart';
+import 'package:family_bottom_sheet/src/common/button/button.dart';
 import 'package:family_bottom_sheet/src/custom_bottom_sheet/widgets/modal_sheet.dart';
+import 'package:family_bottom_sheet/src/presentation/widgets/family_bottom_sheet_shell.dart';
 import 'package:family_bottom_sheet/src/presentation/widgets/private_key_content.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
@@ -10,74 +10,34 @@ class OptionsContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(36)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(24),
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 386),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Options',
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                  CustomBackButton(),
-                ],
-              ),
-              const SizedBox(height: 20),
-
-              // -- Divider
-              Divider(color: Colors.grey.withOpacity(0.2)),
-
-              // -- Button
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: CustomElevatedButton(
-                  onTap: () async {
-                    // await HapticFeedback.lightImpact();
-
-                    FamilyModalSheet.of(
-                      context,
-                    ).pushPage(const PrivateKeyContent());
-                  },
-                  text: 'View Private Key',
-                  icon: IconsaxPlusLinear.lock_1,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: CustomElevatedButton(
-                  onTap: () {
-                    // openModal(context);
-                  },
-                  text: 'View Recovery Phase',
-                  icon: IconsaxPlusLinear.add_circle,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: CustomElevatedButton(
-                  onTap: () {},
-                  text: 'Remove Wallet',
-                  contentColor: Colors.red,
-                  backgroundColor: Colors.red.withOpacity(0.1),
-                  icon: IconsaxPlusLinear.danger,
-                ),
-              ),
-              const SizedBox(height: 10),
-            ],
-          ),
+    return FamilyBottomSheetShell(
+      text: 'Options',
+      children: [
+        CustomTestButton(
+          onTap: () {
+            FamilyModalSheet.of(context).pushPage(const PrivateKeyContent());
+          },
+          text: 'View Private Key',
+          icon: IconsaxPlusLinear.lock_1,
         ),
-      ),
+
+        // CustomTestButton(
+        //   onTap: () {},
+        //   text: 'View Recovery Phase',
+        //   icon: IconsaxPlusLinear.add_circle,
+        // ),
+        CustomTestButton(
+          text: 'Remove Wallet',
+          contentColor: const Color.fromARGB(255, 254, 25, 50),
+          backgroundColor: Color.fromARGB(
+            255,
+            254,
+            25,
+            50,
+          ).withValues(alpha: .2),
+          icon: IconsaxPlusLinear.danger,
+        ),
+      ],
     );
   }
 }
