eddc9eacecbaa7f0efe9a3724dbf83fe20d0fe4b
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-04-04 00:15:37 +0100

    feat: Add FamilyBottomSheetShell and SettingsMenuItem widgets

---
+++ lib/src/presentation/widgets/family_bottom_sheet_shell.dart
@@ -0,0 +1,67 @@
+import 'package:family_bottom_sheet/src/common/button/button.dart';
+import 'package:family_bottom_sheet/src/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class FamilyBottomSheetShell extends StatelessWidget {
+  const FamilyBottomSheetShell({
+    super.key,
+    required this.text,
+    required this.children,
+  });
+
+  final String text;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+
+    return DecoratedBox(
+      decoration: ShapeDecoration(
+        color: colors.surface,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(36)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 386),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // -- Header
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 3,
+                    ),
+                    child: Text(
+                      text,
+                      style: context.textStyles.heading.copyWith(
+                        color: colors.textDefault,
+                      ),
+                      overflow: TextOverflow.clip,
+                      maxLines: 1,
+                    ),
+                  ),
+                  CustomBackButton(),
+                ],
+              ),
+              const SizedBox(height: 20),
+
+              // -- Divider
+              Divider(),
+              const SizedBox(height: 20),
+
+              // -- Content
+              ...children,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
