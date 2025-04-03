30637e77723c27f1f642f3255858c2616b37f462
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-04-04 00:17:49 +0100

    feat: Refactor HomeScreen to include SettingsMenuItem and HelpSupportContent with modal functionality

--- lib/src/presentation/home_screen.dart
+++ lib/src/presentation/home_screen.dart
@@ -1,6 +1,13 @@
-import 'package:family_bottom_sheet/src/custom_bottom_sheet/custom_bottom_sheet.dart';
+import 'package:family_bottom_sheet/src/common/divider/small_divider.dart';
+import 'package:family_bottom_sheet/src/custom_bottom_sheet/widgets/modal_sheet.dart';
+import 'package:family_bottom_sheet/src/presentation/widgets/choose_area_content.dart';
+import 'package:family_bottom_sheet/src/presentation/widgets/help_support_content.dart';
 import 'package:family_bottom_sheet/src/presentation/widgets/options_content.dart';
+import 'package:family_bottom_sheet/src/presentation/widgets/report_bug_content.dart';
+import 'package:family_bottom_sheet/src/presentation/widgets/settings_menu_item.dart';
+import 'package:family_bottom_sheet/src/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax_plus/iconsax_plus.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,23 +20,48 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: OutlinedButton(
-          onPressed: () async {
-            openModal(context);
-          },
-          child: const Text('Open Modal'),
-        ),
+      appBar: AppBar(title: Text('Family Bottom Sheet')),
+      body: ListView(
+        padding: const EdgeInsets.only(left: 24, right: 24, bottom: 48),
+        children: [
+          SettingsMenuItem(
+            title: 'Options',
+            leadingIcon: IconsaxPlusLinear.menu,
+            trailingIcon: IconsaxPlusLinear.arrow_right_3,
+            onTap: () {
+              openOptionsModal(context);
+            },
+          ),
+          const SmallDivider(),
+          SettingsMenuItem(
+            title: 'Helps & Support',
+            leadingIcon: IconsaxPlusLinear.support,
+            trailingIcon: IconsaxPlusLinear.arrow_right_3,
+            onTap: () {
+              openHelpSupportModal(context);
+            },
+          ),
+        ],
       ),
     );
   }
 
-  Future<void> openModal(BuildContext context) async {
+  Future<void> openOptionsModal(BuildContext context) async {
     await FamilyModalSheet.show<void>(
       context: context,
+      contentBackgroundColor: context.colors.surface,
       builder: (ctx) {
         return const OptionsContent();
+      },
+    );
+  }
+
+  Future<void> openHelpSupportModal(BuildContext context) async {
+    await FamilyModalSheet.show<void>(
+      context: context,
+      contentBackgroundColor: context.colors.surface,
+      builder: (ctx) {
+        return const HelpSupportContent();
       },
     );
   }
